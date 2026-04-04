f9da79daf82ec2b6105d231f48f31e41dc151af3
dh3eraj <dheerajgupta200002@gmail.com>
2026-04-04 14:11:07 +0530

    cli implemented

--- index.js
+++ index.js
@@ -6,17 +6,16 @@ const { Chalk } = require("chalk");
 const program = new Command();
 const chalk = new Chalk();
 program
+  .command("start")
   .option("--port <number>", "port number")
   .option(
     "--origin <url>",
     "url of the server for which response needs to be cached",
   )
   .action((options) => {
-    const port = options.port;
+    const port = options.port ;
     const url = options.origin;
     console.log(chalk.blue(`port : ${port} url : ${url}`));
   });
 
 program.parse(process.argv);
-// const options = program.opts();
-// console.log(options.port);
